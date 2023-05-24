620dc1fa91c61a439238e99bae6b305fcf3ee32c
Aozora09 <azharirambe2003@gmail.com>
2023-05-24 13:20:32 +0800

    Project PMB

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 //Klik Gambar ke Link
+function submitform(){
+  alert("Pesan Sudah Dikirim");
+}
 function OpenmyIG() {
   // Logika atau aksi tambahan saat gambar diklik
   window.open('https://www.instagram.com/azhari_aozora/?igshid=ZDc4ODBmNjlmNQ%3D%3D');
